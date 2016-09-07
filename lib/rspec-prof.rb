5ef999a4170a112ceb089bb79c170219e8843ba6
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2016-09-07 13:02:20 -0400

    fix failing test case

--- lib/rspec-prof.rb
+++ lib/rspec-prof.rb
@@ -51,19 +51,21 @@ class RSpecProf
 end
 
 RSpec.configure do |config|
+  profiler = nil
+
   config.before(:suite) do
     unless ['all', 'each', ''].include?(ENV['RSPEC_PROFILE'].to_s)
       raise "ENV['RSPEC_PROFILE'] should be blank, 'all' or 'each', but was '#{ENV['RSPEC_PROFILE']}'"
     end
 
     if ENV['RSPEC_PROFILE'] == 'all'
-      @profiler = RSpecProf.new.start
+      profiler = RSpecProf.new.start
     end
   end
 
   config.after(:suite) do
     if ENV['RSPEC_PROFILE'] == 'all'
-      @profiler.save_to "#{RSpecProf.output_dir}/all.#{RSpecProf.file_extension}"
+      profiler.save_to "#{RSpecProf.output_dir}/all.#{RSpecProf.file_extension}"
     end
   end
 
